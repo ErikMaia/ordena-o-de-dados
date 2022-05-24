ac4d1f943233d5e93c40f35e413317ae61e49c80
Erik Silva Maia <erikm384@gmail.com>
2022-05-24 06:49:07 -0300

    Finish file pesquisa.c

--- pesquisa.c
+++ pesquisa.c
@@ -17,7 +17,9 @@ typedef struct noh Noh;
 int hash(int valor){
     return valor % SIZE;
 }
-show(Noh val[]){
+
+
+void show(Noh val[]){
     for(int i=0; i < SIZE; i++){
         printf(
             "\n %d %s",
@@ -32,12 +34,12 @@ show(Noh val[]){
     printf("\n ---------------------\n");
 }
 
-int insert(Noh vector[], int newval){
-    int hashval = hash(newval);
+int insert(Noh vector[], int val){
+    int hashval = hash(val);
     for(int i = 0; i < SIZE; i++){
         if(vector[(hashval+i)%SIZE].status != OCUPADO){
             vector[(hashval+i)%SIZE].status = OCUPADO;
-            vector[(hashval+i)%SIZE].valor = newval;
+            vector[(hashval+i)%SIZE].valor = val;
             show(vector);
             return hashval+i;
         }
@@ -45,16 +47,65 @@ int insert(Noh vector[], int newval){
     return -1;
 }
 
-int removeNoh(Noh vector[], int removeNohVall){
-    int hashval = hash(removeNohVall);
+int removeNoh(Noh vector[], int val){
+    int hashval = hash(val);
     for(int i = 0; i < SIZE && vector[(hashval+i)%SIZE].status != FREE; i++){
-        if(vector[(hashval+i)%SIZE].valor == removeNohVall){
+        if(vector[(hashval+i)%SIZE].valor == val){
             vector[(hashval+i)%SIZE].status = REMOVIDO;
             show(vector);
             return hashval+i;
         }
     }
     return -1;
+}
+
+int addqt(Noh vector[], int val){
+    int hashval = hash(val);
+    for(int i = 0; i < SIZE; i++){
+        if(vector[(hashval+pow(i,2))%SIZE].status != OCUPADO){
+            vector[(hashval+pow(i,2))%SIZE].status = OCUPADO;
+            vector[(hashval+pow(i,2))%SIZE].valor = val;
+            show(vector);
+            return hashval+i;
+        }
+    }
+    return -1;
+}
+
+int removeqt(Noh vector[], int val){
+    int hashval = hash(val);
+    for(int i = 0; i < SIZE && vector[(hashval+i)%SIZE].status != FREE; i++){
+        if(vector[(hashval+i)%SIZE].valor == val){
+            vector[(hashval+i)%SIZE].status = REMOVIDO;
+            show(vector);
+            return hashval+i;
+        }
+    }
+    return -1;
+}
+
+int adduni(Noh vector[], int val, int *size){
+    int p = 13;
+    int m = 8;
+    int a = 5;
+    int b = 7;
+    int state = 0;
+    int hash;
+    if(sizeof(vector)/sizeof(Noh) < size){
+        return - 1;
+    }
+    do{
+        hash = !state ? ((a*val+b)%p)%m : ((a*hash+b)%p)%m;
+        state = 1;
+        if(vector[hash].status != OCUPADO){
+            vector[hash].valor = val;
+            show(vector);
+            *size++; 
+            return hash;
+        }
+    }
+    while(1);
+
 }
 
 int main(){
